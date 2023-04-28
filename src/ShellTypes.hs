4ce46eb5fbac82f86c197e55fb340b2aeee0f984
Christoffer Lind Andersen <chri692u@gmail.com>
2023-04-28 17:18:08 +0200

    Cursor added

--- src/ShellTypes.hs
+++ src/ShellTypes.hs
@@ -3,10 +3,12 @@ module ShellTypes where
 import System.Console.Repline
 import Control.Monad.State
 
-data FileSystemTree = FileNode FilePath | DirNode FilePath [FileSystemTree]
+data Directory = DirNode FilePath [FileSystemTree]
+data FileSystemTree = FileNode FilePath | Dir Directory
 
-newtype IState = IState { 
-        fs :: FileSystemTree
+data IState = IState { 
+        fs :: FileSystemTree ,
+        cursor :: Directory
     }
 
 type Repl a = HaskelineT (StateT IState IO) a
