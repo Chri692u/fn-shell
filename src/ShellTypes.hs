3370db1c02924fdbcdd3adeee59574be89c7ce74
Christoffer Lind Andersen <chri692u@gmail.com>
2023-04-28 19:26:56 +0200

    Refactor and new cursor

--- src/ShellTypes.hs
+++ src/ShellTypes.hs
@@ -3,12 +3,27 @@ module ShellTypes where
 import System.Console.Repline
 import Control.Monad.State
 
+-- Types for the file system
 data Directory = DirNode FilePath [FileSystemTree]
 data FileSystemTree = FileNode FilePath | Dir Directory
 
+-- Shell state
 data IState = IState { 
         fs :: FileSystemTree ,
-        cursor :: Directory
+        cursor :: [Directory]
     }
 
+-- Shell Type
 type Repl a = HaskelineT (StateT IState IO) a
+
+-- Get the directory from the file system
+fsDir :: FileSystemTree -> Directory
+fsDir (Dir dir) = dir
+
+-- Get directory name
+dirName :: Directory -> FilePath
+dirName (DirNode name _) = name
+
+-- Get the list of files in a directory
+dirTree :: Directory -> [FileSystemTree]
+dirTree (DirNode _ fs) = fs  
