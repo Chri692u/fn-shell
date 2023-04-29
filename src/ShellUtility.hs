169caf670bd985bb545e1b0a0b000e63038bc84f
Christoffer Lind Andersen <chri692u@gmail.com>
2023-04-29 16:43:48 +0200

    printing

--- src/ShellUtility.hs
+++ src/ShellUtility.hs
@@ -5,9 +5,10 @@ import ShellTypes (IState)
 
 initMsg :: IO ()
 initMsg = do 
-    putStrLn "(fn shell) version 0.1.0.0"
-    putStrLn ":help - a list of all features"
-    putStrLn "Ctrl + D will quit the shell"
+    putStrLn "   (fn shell) version 0.1.0.0   "
+    putStrLn "--------------------------------"
+    putStrLn ":help - a list of all features  "
+    putStrLn "Ctrl + D will quit the shell    "
 
 allCmds :: [String]
 allCmds = [":help", ":quit", ":cat", ":pwd", ":ls", ":cd"]
